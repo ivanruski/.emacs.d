f93bd3e03c0849ffef4957fcfe8fc2a9f42cc26b
ivanruski <ruski9606@gmail.com>
2018-10-09 10:24:16 +0300

    New lines to my init.el + racket-mode package

--- init.el
+++ init.el
@@ -1,7 +1,9 @@
 ;; Hide the toolbar
 (tool-bar-mode -1)
 
-(setq inhibit-startup-screen t)
+;; Hide the menu bar
+(menu-bar-mode -1)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -9,12 +11,18 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
- '(custom-enabled-themes (quote (deeper-blue))))
+ '(custom-enabled-themes (quote (wombat)))
+ '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/")
+             t)
+
+(setq racket-program "./../../usr/bin/racket")
