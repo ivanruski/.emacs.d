51fd6b1ec8621451f5261919aa33ffb590f92712
Ivan Ivanov <ivan.ivanov@code-runners.com>
2020-08-15 19:20:58 +0300

    Use default configuration for backup files

--- init.el
+++ init.el
@@ -58,14 +58,9 @@
 ;;
 (global-set-key (kbd "C-<tab>") 'switch-to-buffer)
 
-;; Set emacs backup directory
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
-  backup-by-copying t    ; Don't delink hardlinks
-  version-control t      ; Use version numbers on backups
-  delete-old-versions t  ; Automatically delete excess backups
-  kept-new-versions 2   ; how many of the newest versions to keep
-  kept-old-versions 2    ; and how many of the old
-  )
+;; backup files
+(setq backup-directory-alist
+      `((".*" "~/.emacs.d/backup/" t)))
 
 ;; use spaces instead of tab characters
 (setq-default indent-tabs-mode nil)
